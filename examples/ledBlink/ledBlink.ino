0a57a63068ce4e4b808cb5b397af9ff01a617f84
Luka Markovic <luka.markovic276@gmail.com>
2024-04-08 13:48:36 +0200

    initial commit and ledBlink example

---
+++ examples/ledBlink/ledBlink.ino
@@ -0,0 +1,15 @@
+#include "Nicla_System.h"
+
+void setup() {
+  nicla::begin();
+  nicla::leds.begin();
+}
+
+void loop() {
+  nicla::leds.setColor(red);
+  delay(1000);
+  nicla::leds.setColor(green);
+  delay(1000);
+  nicla::leds.setColor(blue);
+  delay(1000);
+}
